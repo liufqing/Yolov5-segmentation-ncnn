390da2c271b1f425a09f1b8f07bbf327a1faf5ae
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-06-06 18:27:37 +0700

    bruh

--- Yolov5-seg/source code/files.cpp
+++
@@ -1,7 +0,0 @@
-#include "files.h"
-
-void mkdir(std::string& fileName) {
-	auto makeDir = std::filesystem::create_directories(fileName);
-	if (makeDir)
-		std::cout << "\nCreated " << fileName;
-}
--- Yolov5-seg/source code/files.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <filesystem>
-#include <iostream>
-
-void mkdir(std::string& fileName);
