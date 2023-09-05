803d1d8201d9dfdeb6d1562b688268f02351a57e
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-09-05 16:16:25 +0700

    edit autogenrotate

--- Tools/autoGenRotate/autoGenRotate.cpp
+++ Tools/autoGenRotate/autoGenRotate.cpp
@@ -8,34 +8,31 @@
 using std::cout;
 using std::endl;
 
-int main(int argc, char* argv[]){
-	const char* input = argv[1];
-	const char* minAngle = argv[2];
-	const char* maxAngle = argv[3];
-	const char* stepAngle = argv[4];
-	const char* scale = argv[5];
+int main(int argc, char* argv[]) {
+    const char* input = argv[1];
+    const char* minAngle = argv[2];
+    const char* maxAngle = argv[3];
+    const char* stepAngle = argv[4];
 
-	int min = std::stoi(minAngle);
-	int max = std::stoi(maxAngle);
-	int step = std::stoi(stepAngle);
+    int min = std::stoi(minAngle);
+    int max = std::stoi(maxAngle);
+    int step = std::stoi(stepAngle);
 
-	cv::Mat src = cv::imread(input);
+    cv::Mat src = cv::imread(input);
 
-	std::filesystem::path inputPath = input;
+    std::filesystem::path inputPath = input;
 
-	cv::Mat dst;
-	cv::Size size(src.cols,src.cols);
-	cv::Point2f center(src.cols / 2, src.rows / 2);
-	std::string outputFolder = inputPath.parent_path().string() + "\\" + inputPath.stem().string();
-	cv::utils::fs::createDirectory(outputFolder);
-	for (int i = min; i <= max; i+=step) {
-		cv::Mat affineMatrix = cv::getRotationMatrix2D(center, i, std::stof(scale));
-		cv::warpAffine(src, dst, affineMatrix, src.size(), cv::INTER_NEAREST, cv::BORDER_CONSTANT);
-		cout << "Generating " << std::to_string(i) + ".jpg" << endl;
-		cv::imwrite(outputFolder + "/" + std::to_string(i) + ".jpg", dst);
-	}
-	cout << (max - min)/step + 1 << " images Generated are save to " << outputFolder <<endl;
-	cout << "------------------------------------------------" << std::endl;
+    cv::Mat dst;
+    cv::Size size(src.cols, src.cols);
+    std::string outputFolder = inputPath.parent_path().string() + "\\" + inputPath.stem().string();
+    cv::utils::fs::createDirectory(outputFolder);
+    for (int i = min; i <= max; i += step) {
+        dst = imutils::rotateBounds(src, i);
+        cout << "Generating " << std::to_string(i) + ".jpg" << endl;
+        cv::imwrite(outputFolder + "/" + std::to_string(i) + ".jpg", dst);
+    }
+    cout << (max - min) / step + 1 << " images Generated are save to " << outputFolder << endl;
+    cout << "------------------------------------------------" << std::endl;
 
-	return 0;
+    return 0;
 }
